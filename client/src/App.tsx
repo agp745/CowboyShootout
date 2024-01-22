33516adf7166ac5db0b7a41250a0c6b3d1ebe5f5
Alex Perez <alejandro.gperez745@gmail.com>
2024-01-22 17:36:49 -0600

    Server and Client setup

---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+export default function App() {
+  const socket = new WebSocket("ws://127.0.0.1:8080");
+
+  socket.addEventListener("open", (e) => {
+    socket.send("connection established");
+    console.log(`open event\n ${e}`);
+  });
+  socket.addEventListener("message", (e) => {
+    console.log("Message from server: ", e.data);
+  });
+  socket.addEventListener("error", (e) => {
+    console.log("WEBSOCKET ERROR", e);
+  });
+
+  return (
+    <div className="bg-black/90 flex flex-col h-screen justify-center items-center text-white">
+      Hello World!
+      <button>HI</button>
+    </div>
+  );
+}
